25582b9bec5b6dfce307512833de0f0679c8d6c7
dd482IT <dd482@njit.edu>
2021-01-28 22:32:46 -0500

    json_basics.py

--- json_basics.py
+++ json_basics.py
@@ -1,6 +1,7 @@
 import json 
 
-#json to Python ---------------------------------- 
+#json to Python ----------------------------------
+print("Here we convert Json to Python") 
 x = '{"name":"John", "age":30, "city":"New York"}'
 
 #Parse it here
@@ -9,10 +10,11 @@ y = json.loads(x)
 #Print out python dictionary 
 print(y["age"])
 
-#Python to json ---------------------------------
 
+
+#Python to json ---------------------------------
+print("Here we convert Python to json") 
 x = {
-    
     "name": "John",
     "age": 30, 
     "city": "New York" 
@@ -23,4 +25,35 @@ y = json.dumps(x)
 
 #JSON string result
 print(y)
+
+
+#Conversions from Python to JSON 
+
+#dict   Object 
+#list   Array 
+#tuple  Array 
+#str    String 
+#int    Number 
+#float  Number 
+#True   true 
+#False  false 
+#None   null 
+
+#Convert a python object containing all the legal data types: 
+
+print("Here we convert a python object containing all the legal data types") 
+x = {
+    "name": "John", 
+    "age": 30, 
+    "married": True,
+    "divorced": False,
+    "children": ("Ann", "Billy"), 
+    "pets": None, 
+    "cars": [
+        {"model": "BMW 230", "mpg": 27.5},
+        {"model": "Ford Edge", "mpg": 24.1}
+    ]
+}
+
+print(json.dumps(x))
 
