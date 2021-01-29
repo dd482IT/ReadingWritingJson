c45cabf3ff851365a1c0dcdb2f72c1f6ce2f1ee0
dd482IT <dd482@njit.edu>
2021-01-28 23:21:17 -0500

    Takes users.json and modifies the password

--- json_reader.py
+++ json_reader.py
@@ -1,7 +1,18 @@
+#this small program takes json data from a file and changes each usernames password to hahahahha
+#original code is from Kalle Hallden from his video "Python Projects For Beginners"
+
+#first the file is opened using open and each data point is kept as f 
+#users are loaded in as f and an attribute is pulled
+#the password attribute is changed
+#the new value for password is then jumped into users stored as f
+
+
+
 import json
 
+json_file = input("insert the file name: ")
 users = {} 
-with open('users.json') as f: 
+with open(json_file) as f: 
     users = json.load(f)
 print(users['username'])
 print(users['password'])
